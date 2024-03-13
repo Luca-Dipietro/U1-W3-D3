31eb2d9f51a44e80cc50f5df7904cbabcef76ec0
Luca <luca.dipietro.ld@gmail.com>
2024-03-13 18:21:13 +0100

    Complete the todolist

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-const addNewTask = function () {
-  const newLi = document.createElement("li");
-  const inputValue = document.getElementById("new-task").value;
-  const stringInputValue = document.createTextNode(inputValue);
-  newLi.appendChild(stringInputValue);
-  if (inputValue === "") {
-    alert("!Write something!");
-  } else {
-    document.getElementById("task-list").appendChild(newLi);
-    const span = document.createElement("span");
-    span.innerHTML = "X";
-    newLi.appendChild(span);
-  }
-  document.getElementById("new-task").value = "";
-};
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("task-list");
 
-const listContainer = document.getElementById("list-container");
-listContainer.addEventListener(
-  "click",
-  function (event) {
-    if (event.target.tagName === "li") {
-      event.target.classList.toggle("checked");
-    } else if (event.target.tagName === "span") {
-      event.target.parentElement.remove();
-    }
-  },
-  false
-);
+const addTask = function () {
+  const inputText = inputBox.value;
+  if (inputText !== "") {
+    let newLi = document.createElement("li");
+    newLi.textContent = inputText;
+    newLi.addEventListener("click", function () {
+      newLi.classList.toggle("checked");
+    });
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", function () {
+      listContainer.removeChild(newLi);
+    });
+    newLi.appendChild(deleteBtn);
+    listContainer.appendChild(newLi);
+  } else {
+    alert("!Write Something!");
+  }
+  inputBox.value = "";
+};
